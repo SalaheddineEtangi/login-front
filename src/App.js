8d22ea51bfa127071a39b8c6f996a09851ffff50
salah-eddine.etangi <salah-eddine.etangi@cdbdx.biz>
2021-05-10 16:26:12 +0200

    login-front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import logo from './akka.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div className = "App" >
+            <header className = "App-header" >
+                <img src = { logo } className = "App-logo" alt = "logo" / >
+                <p>
+                    Login application. 
+                </p> 
+                <a className = "App-link"
+                href = "https://reactjs.org"
+                target = "_blank"
+                rel = "noopener noreferrer" >
+                    Get source code 
+                </a> 
+            </header> 
+        </div>
+    );
 }
 
 export default App;
