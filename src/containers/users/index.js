e0709c64c939ace5767c81919cec1efa2fc70271
salah-eddine.etangi <salah-eddine.etangi@cdbdx.biz>
2021-05-12 17:50:11 +0200

    login-front

--- src/containers/users/index.js
+++ src/containers/users/index.js
@@ -4,29 +4,53 @@ import UsersList from '../../components/UsersList'
 class Users extends Component {
     state = {
         collectionName: "",
-        collection: []
+        collection: [],
+        isFetching: false
     }
 
     onUserInputChange = e => {
+        this.setState({
+            collectionName: e.target.value,
+            isFetching: true
+        })
         fetch(`http://localhost:52935/${e.target.value}`)
         .then(response => response.json())
         .then(json => this.setState(
             {
-                collectionName: e.target.value, 
-                collection: json.items
+                collection: json.items,
+                isFetching: false
             }));
     }
 
     render(){
+        const {collectionName, collection, isFetching} = this.state;
+
         return(
             <div>
-                <p>
-                    There are {this.state.collection.length} {this.state.collectionName} registered to the application
-                </p>
                 <div>
-                    <input type="text" onChange={this.onUserInputChange}/>
+                    <input
+                        value={this.state.collectionName} 
+                        type="text" 
+                        onChange={this.onUserInputChange}
+                    />
                 </div>
-                <UsersList list={this.state.collection}/>
+                {
+                    collection.length === 0 && collectionName.trim() === ''
+                    &&
+                    <p>Entrez le nom de la collection ?</p>
+                }
+                {
+                    !isFetching && collection.length === 0 && collectionName.trim() !== ''
+                    &&
+                    <p>Il n y a pas de collections avec ce nom ?</p>
+                }
+                {
+                    isFetching && collectionName.trim() !== '' 
+                    && <p>Chargement...</p>
+                }
+                {
+                    !isFetching && <UsersList list={this.state.collection}/>
+                }
             </div>
         )
     }
